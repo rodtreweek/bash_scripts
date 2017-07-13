3684ac2f9a71c4c9621204a969cbd4be9dff46b4
Rod Treweek <rod@puppet.com>
2017-07-13 07:49:10 -0700

    Adding comments

--- mysql_grab.sh
+++ mysql_grab.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# setup the shell environment (set -e and set -o pipefail)
 set -e
 set -o pipefail
+
+# As root (via sudo) execute ngrep (presumably gnu's ngrep) 
+# to grab the output of the mysql SQL statement looking for 
+# the tcp info on the interface to the DB
 exec sudo ngrep -P ' ' -l -W single -d bond0 -q 'SELECT' 'tcp and dst port 3306' |
   egrep "\[AP\] .\s*SELECT " |
   sed -e 's/^T .*\[AP\?\] .\s*SELECT/SELECT/' -e 's/$/;/' |
